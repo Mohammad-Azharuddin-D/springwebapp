cac5fe3a8d38e5513d5e41abcf3d3784bf7cc509
Azhar <50619772+Mohammad-Azharuddin-D@users.noreply.github.com>
2025-04-15 16:24:30 +0530

    Add files via upload

---
+++ src/main/java/com/azhar/springwebapp1/Springwebapp1Application.java
@@ -0,0 +1,13 @@
+package com.azhar.springwebapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springwebapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springwebapp1Application.class, args);
+	}
+
+}
